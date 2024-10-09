7ad66723f0541d3e738b02e41104b74322361c7f
MarcoTz <marco.tzschentke@gmail.com>
2024-10-09 10:38:23 +0200

    removed helper structs

--- lib/algebra/src/ring.rs
+++ lib/algebra/src/ring.rs
@@ -11,36 +11,3 @@ pub trait Ring: Group + Mul<Output = Self> + Clone {
         }
     }
 }
-
-#[derive(PartialEq)]
-pub struct AsGroup<T: Ring> {
-    elem: T,
-}
-
-impl<T: Ring> From<T> for AsGroup<T> {
-    fn from(elem: T) -> AsGroup<T> {
-        AsGroup { elem }
-    }
-}
-
-impl<T: Ring> Add for AsGroup<T> {
-    type Output = AsGroup<T>;
-    fn add(self, other: AsGroup<T>) -> AsGroup<T> {
-        AsGroup {
-            elem: self.elem + other.elem,
-        }
-    }
-}
-
-impl<T: Ring> Group for AsGroup<T> {
-    fn zero() -> AsGroup<T> {
-        AsGroup {
-            elem: <T as Group>::zero(),
-        }
-    }
-    fn neg(self) -> AsGroup<T> {
-        AsGroup {
-            elem: <T as Group>::neg(self.elem),
-        }
-    }
-}
