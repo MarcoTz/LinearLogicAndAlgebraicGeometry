00a70127ed47deaf9ec6e52b0bdbc4f4ce54b3b6
MarcoTz <marco.tzschentke@gmail.com>
2024-10-09 10:40:12 +0200

    added nonabelian group

--- lib/algebra/src/group.rs
+++ lib/algebra/src/group.rs
@@ -1,6 +1,11 @@
-use std::ops::Add;
+use std::ops::{Add, Mul};
 
 pub trait AbelianGroup: Add<Output = Self> + PartialEq + Sized {
     fn zero() -> Self;
     fn neg(self) -> Self;
+}
+
+pub trait Group: Mul<Output = Self> + PartialEq + Sized {
+    fn one() -> Self;
+    fn inverse(self) -> Self;
 }
